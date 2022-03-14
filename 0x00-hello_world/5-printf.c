1140647b28795df612ffcb32af385a80120c0078
daisyarusey <daisyarusey.dc@gmail>
2022-03-14 12:47:47 -0700

    update script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
--e#include <stdio.h>\n/**\n* main - Entry point\n*\n* Return: Always 0 (Success)\n*/\nint main(void)\n{\nprintf("with proper grammar, but the outcome is a piece of art,\n");\nreturn (0);\n}
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
