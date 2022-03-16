4228dddda10478a816c7de3ee24907a189594dba
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-16 13:24:29 -0700

    Complete task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*  Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	int i;
+	char str[] ="_putchar";
+	for(i=0;str[i];i++)
+		printf("%c",str[i]);
+	printf("\n";
+	return(0);
+}
