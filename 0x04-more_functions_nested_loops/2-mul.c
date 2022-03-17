f6e46b831175a743d64c24a8db9d0a10458fa0b3
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-17 14:40:53 -0700

    Fix betty documentation

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * int mul - multiply the inputs .
+ * mul - multiply the inputs .
  *
  *  @a: First integer
- *  @a: Second integer
+ *  @b: Second integer
  * Return: returns products of a and b
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int res;
+
+	res = a * b;
+return (res);
 }
