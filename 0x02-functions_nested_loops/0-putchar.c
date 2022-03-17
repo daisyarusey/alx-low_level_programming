80ba0a9e2f8bf734f8e7aad5f78af7287065b863
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-17 13:20:29 -0700

    Modify

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,14 @@
 int main(void)
 {
 
-	int i;
-	char str[] = "putchar";
-
-	for (i = 0; i < 7; i++)
-		_putchar(str[i]);
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
