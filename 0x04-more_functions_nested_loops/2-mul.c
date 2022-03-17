8342ce7bb8021c87bf3174e2e9ed61cf1904e345
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-17 14:32:15 -0700

    Complete task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * int mul - multiply the inputs .
+ *
+ *  @a: First integer
+ *  @a: Second integer
+ * Return: returns products of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
