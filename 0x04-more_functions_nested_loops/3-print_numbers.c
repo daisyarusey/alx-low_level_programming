7bb560da26497a82f9d49a141eedabf5ecb43930
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-17 14:54:09 -0700

    Fix code

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9.
- * Return: Always 0.
+ *
  */
 void print_numbers(void)
 {
@@ -11,5 +11,4 @@ void print_numbers(void)
 	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
 	_putchar('\n');
-	return (0);
 }
