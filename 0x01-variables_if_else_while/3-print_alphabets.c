8bc44dbc6a1a9c05d76c0af4ec391e86c6ddae7e
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-17 01:08:47 -0700

    Complete task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower and uppercase
+ *
+ * Return: Aways 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for(c='a';c<='z';c++)
+		putchar(c);
+
+	for(c='A';C='Z';c+=)
+		putchar(C);
+	putchar('\n');
+	return (0);
+}
