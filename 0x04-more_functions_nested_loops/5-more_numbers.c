c5a166e78ac182203a95fd42773c46a954a897d2
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-21 00:15:10 -0700

    Solve errors

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,14 @@
 void more_numbers(void)
 {
 
-	char num;
+	char c;
 	int i;
 
 	i = 0;
 
 	while (i < 10)
 	{
-		for (num = 0; num <= 14; num++)
+		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
 				_putchar((c / 10) + '0');
