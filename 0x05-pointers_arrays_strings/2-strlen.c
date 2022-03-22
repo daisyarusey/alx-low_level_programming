b202ab333afba16ab4cabca89affd8ef02f18748
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-22 10:01:32 -0700

    Complte task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,8 +6,10 @@
  */
 int _strlen(char *s)
 {
-	int len = 0;
-	
+	int len a = 0;
+
+
+
 	while (*s != '\0')
 	{
 		s++;
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string.
+ * @str: value to be evaluated.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (*(str + i) != '\0)
+	{
+		_putchar(str[i];
+		i++;
+	}
+	_putchar('\n');
+}
