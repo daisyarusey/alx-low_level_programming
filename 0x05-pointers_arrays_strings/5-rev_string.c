28530d39ae78282220f2c58b79266b0c3eab1f02
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-23 14:23:23 -0700

    change from for loop to while loop

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,17 @@
 void rev_string(char *s)
 {
 	int len = 0;
-	int i;
+	int i = 0;
 	char temp;
 
 	while (s[len] != '\0')
 		len++;
-
-	for (i = 0; i < len; i++)
+	while (i < len)
 	{
 		temp = s[i];
 		s[i] = s[len];
 		s[len] = temp;
 		len--;
+		i++;
 	}
 }
