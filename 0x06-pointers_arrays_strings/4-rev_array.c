9b66f4fc5600e25f2e10baed377e671d2ae2c271
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-24 13:24:00 -0700

    Complete task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array
+ * @a: input array
+ * @n: length of array.
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp;
+
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+
+		i++;
+		n++;
+	}
+}
