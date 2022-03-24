fe017272862c636492de63ce913c17ca4233c30b
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-24 01:30:01 -0700

    Fix error

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,6 +2,7 @@
 #include <math.h>
 /**
  * main - finds and prints the largest prime factor of the number 612852475143.
+ * Return: always 0 (success).
  */
 
 int main(void)
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,6 +19,6 @@ void print_number(int n)
 	if (k != 0)
 		print_number(k);
 
-	_putchar((unsigned int) n 5 % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
 
 }
