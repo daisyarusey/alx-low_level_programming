398529446b2afca2a9fce0f36c9a4464e0099405
daisyarusey <daisyarusey.dc@gmail.com>
2022-03-28 13:01:54 -0700

    Complete task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * Return: memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a]
+	}
+	return (dest);
+}
